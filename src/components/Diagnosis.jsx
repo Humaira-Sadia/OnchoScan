336d74d78bf29ca283e124cef43bab3ba8ccf8a5
Humaira Sadia <humaira.sadia@streebo.com>
2026-05-28 18:17:33 +0530

    added backend url

--- src/components/Diagnosis.jsx
+++ src/components/Diagnosis.jsx
@@ -90,7 +90,7 @@ export const Diagnosis = ({ onComplete }) => {
 
             reader.onload = async () => {
                 try {
-                    const response = await fetch("http://127.0.0.1:5000/predict", {
+                    const response = await fetch("https://onchoscan-backend.onrender.com/predict", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json",
